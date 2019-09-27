9d928f2f398e459f2f144b112be9fb56d9f1ba45
Glitch (zgleman-express) <none>
2019-09-27 09:24:52 +0000

    👯🌴 Checkpoint ./myApp.js:7037687/256

--- myApp.js
+++ myApp.js
@@ -50,14 +50,17 @@ process.env.MESSAGE_STYLE = 'uppercase';
 app.get("/now", function(req, res, next){
   req.time = new Date().toString();
   next();
-}
+},
 function(req, res){
   res.json({"time": req.time});
 }
-)
+);
 
 /** 9)  Get input from client - Route parameters */
 
+app.get(":word/echo", function(req, res){
+  res.json({"echo": req.params.word});
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
